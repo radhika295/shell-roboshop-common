4d60cb4e06f84820a48e460a5e9e24c93c8a1ee7
Radhika <sairadhika.iiitn@gmail.com>
2025-09-29 00:04:42 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -15,7 +15,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
 INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
+if ["${INDEX:-999}" -le 0]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load $app_name products"
 else
